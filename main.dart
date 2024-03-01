11cb0c4ac8ab16fddc795dca68eae2533ef27317
Tomas <99651044+Oryares@users.noreply.github.com>
2024-03-01 22:08:02 +0300

    new card app

---
+++ main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:card/home.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Basic CardApp - Thomas',
+      home: Home(),
+    );
+  }
+}
+
